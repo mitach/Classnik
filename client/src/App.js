135f1d8aa809282b717e093c8534e17ff0769f37
mitach <malinovd02@gmail.com>
2022-07-19 19:12:36 +0300

    Added routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,20 @@
+import { Routes, Route, Navigate } from 'react-router-dom';
+
+import Auth from './components/Auth/Auth';
 import Layout from "./components/Layout/Layout";
 
 function App() {
   return (
     <div className="classnik-app">
-      <Layout />
+      <Routes>
+
+        <Route path='/' element={loggedIn ? <Navigate to='/dashboard' /> : <Auth />} />
+
+        <Route path='/login' element={loggedIn ? <Navigate to='/dashboard' /> : <Auth />} />
+        <Route path='/register' element={loggedIn ? <Navigate to='/dashboard' /> : <Auth />} />
+
+        <Route path='/dashboard/*' element={loggedIn ? <Layout /> : <Auth />} />
+      </Routes>
     </div>
   );
 }
